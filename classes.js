61a43844decfb97b709c78220c53c69c18f93917
MarleyDouglas007 <marley.santos2308@gmail.com>
2023-09-28 14:35:44 -0300

    classe participante

--- classes.js
+++ classes.js
@@ -1,28 +1,37 @@
-class Aluno{
-    constructor(nome,cpf,dataDeNacimento,matricula){
-        this.nome = nome
-        this.cpf = cpf
-        this.dataDeNacimento = dataDeNacimento
-        this.matricula = matricula
+class Participante {
+    constructor(nome, cpf, dataDeNacimento){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataDeNacimento = dataDeNacimento;
     }
 
 }
-class Professor{
-    constructor(nome,cpf,dataDeNacimento,siape,formacao){
-        this.nome = nome
-        this.cpf = cpf
-        this.dataDeNacimento = dataDeNacimento
-        this.siape = siape
-        this.formacao = formacao
+
+class Aluno {
+    constructor(nome, cpf, dataDeNacimento, matricula) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataDeNacimento = dataDeNacimento;
+        this.matricula = matricula;
+    }
+
+}
+class Professor {
+    constructor(nome, cpf, dataDeNacimento, siape, formacao) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataDeNacimento = dataDeNacimento;
+        this.siape = siape;
+        this.formacao = formacao;
     }
 }
 
-class Turma{
-    constructor(){
+class Turma {
+    constructor() {
         this.listaDeParticipantes = [];
 
     }
-    adicionarParticipante(participante){
+    adicionarParticipante(participante) {
         this.listaDeParticipantes.push(participante)
     }
 }
